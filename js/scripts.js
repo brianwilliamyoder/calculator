c2c9abd07b0c43053ba14b80d27d2eea0d693ed0
Brian Yoder <brianyoder@gmail.com>
2022-10-26 19:40:55 -0700

    add substract, divide, and multiply functions, add window alert for all operators

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,22 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
 const number1 = parseInt(prompt('Enter a number:'));
 const number2 = parseInt(prompt('Enter another number:'));
 
-window.alert(add(number1, number2));
+window.alert(number1.toString() + "+" + number2.toString() + "=" + add(number1, number2) + "." +  
+" The difference of your numbers is " + subtract(number1, number2) + "." +
+" The product of your numbers is " + multiply(number1, number2) + "." + 
+" The quotient of your numbers is " + divide(number1, number2) + ".");
