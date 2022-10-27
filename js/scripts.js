b97a7065ff6ad39754e3548c62a2e11d0dbb4b0b
Brian Yoder <brianyoder@gmail.com>
2022-10-26 19:14:53 -0700

    add number 1 and number 3 variables, add window alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
 
+//business logic
 function add(number1, number2) {
   return number1 + number2;
 }
+
+const number1 = parseInt(prompt('Enter a number:'));
+const number2 = parseInt(prompt('Enter another number:'));
+
+window.alert(add(number1, number2));
